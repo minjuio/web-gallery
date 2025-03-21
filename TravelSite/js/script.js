be967512f4235410d346aa6cf672d4ec59d2043f
Lee Min Ju <69198709+minjuio@users.noreply.github.com>
2025-03-21 16:19:07 +0900

    Add files via upload

---
+++ TravelSite/js/script.js
@@ -0,0 +1,26 @@
+$(function(){
+  // menu
+  $("nav>ul>li").mouseenter(function(){
+    $(this).children(".sub").stop().slideDown();
+  })
+  $("nav>ul>li").mouseleave(function(){
+    $(".sub").stop().slideUp();
+  })
+
+  // slide
+  let i = 0;
+
+  function slide(){
+    if(i<2) {
+      i++;
+    } else {
+      i = 0;
+    }
+    $(".slide ul").animate({marginLeft: -1200 * i}, 1000);
+  }
+  setInterval(slide, 3000);
+
+
+
+  
+})
